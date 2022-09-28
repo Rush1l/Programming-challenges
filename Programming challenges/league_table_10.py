7d5997d9d1afedb50cf640e9a3ae8c922fb14234
Rush1l <113171596+Rush1l@users.noreply.github.com>
2022-09-28 14:44:35 +0100

    Update league_table_10.py

--- Programming challenges/league_table_10.py
+++ Programming challenges/league_table_10.py
@@ -1,21 +1,3 @@
-import csv 
-from pathlib import Path 
-
-csv_file = Path("Premier 16-17.csv")
-
-def check_file_exists(csv_file): 
-    return csv_file.is_file()
-        
-def read_csv(csv_file):
-    csv_contents = []
-    if check_file_exists(csv_file):
-        with open(csv_file) as csvfile:
-            reader = csv.reader(csvfile, delimiter=",")
-            next(reader)                  ###   skip first row (header)
-            for row in reader:
-                csv_contents.append(row)
-    return csv_contents
-
 def process_results(rows):
     dictionary = {}
     for row in rows:
@@ -45,7 +27,7 @@ def process_results(rows):
             dictionary[home][2]+=1
             dictionary[away][0]+=1
 
-            dictionary[away][4]+=1
+            dictionary[away][4]+=3
 
 
 
@@ -55,14 +37,14 @@ def process_results(rows):
             dictionary[home][0]+=1
             dictionary[away][2]+=1
 
-            dictionary[home][4]+=1
+            dictionary[home][4]+=3
 
 
         dictionary[home][3] += int(home_goals) - int(away_goals)
         dictionary[away][3] += int(away_goals) - int(home_goals)
     #print(dictionary)
-    for y, z in dictionary.items():
-        print(y, z)
+    return dictionary
+
 
 
 
@@ -72,6 +54,17 @@ def process_results(rows):
 
 if __name__ == "__main__":
     file_contents = read_csv(csv_file)
-    process_results(file_contents)
+    myDict=process_results(file_contents)
+    print(f"{'Teams':<20}{'Wins':<10}{'Draws':<10}{'Losses':<10}{'Goal diff':<13}{'Points':<10}")
+    print("")
+    for key, value in sorted(myDict.items(), key=lambda e: e[1][4],reverse=True):
+        print(f'{key:<20} {value[0]:<10} {value[1]:<10} {value[2]:<10} {value[3]:<10} {value[4]:<10}')
+   
+ 
+
+
+ 
+
+
 
  
